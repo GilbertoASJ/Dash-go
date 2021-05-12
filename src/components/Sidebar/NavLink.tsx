d99a364c0a5e04357a27c22df100d8e73ceea07b
GilbertoASJ <alvesgilberto84@gmail.com>
2021-05-12 17:21:28 -0300

    :sparkles: Updating file

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,8 @@
 import { Text, Link as ChakraLink, Icon, LinkProps as ChakraLinkProps } from '@chakra-ui/react'
 import { ElementType } from 'react'
 
+import { ActiveLink } from '../ActiveLink'
+
 import Link from 'next/link'
 
 interface NavLinkProps extends ChakraLinkProps {
@@ -11,11 +13,11 @@ interface NavLinkProps extends ChakraLinkProps {
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
 	return (
-		<Link href={href} passHref>
+		<ActiveLink href={href} passHref>
 			<ChakraLink display="flex" align="center" {...rest}>
 				<Icon as={icon} fontSize="20" />
 				<Text ml="4" fontWeight="medium">{children}</Text>
 			</ChakraLink>
-		</Link>
+		</ActiveLink>
 	)	
 }
