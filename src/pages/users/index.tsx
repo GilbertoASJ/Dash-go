66e79333fab644fddbb89da806f8d6b76ffd55d4
GilbertoASJ <alvesgilberto84@gmail.com>
2021-05-26 17:12:21 -0300

    :sparkles: Updating files

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -9,6 +9,7 @@ import { Header } from '../../components/Header'
 import { Sidebar } from '../../components/Sidebar'
 import { Pagination } from '../../components/Pagination'
 
+import { useEffect } from 'react'
 import Link from 'next/link'
 
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
@@ -19,6 +20,12 @@ export default function UserList() {
 		base: false,
 		lg: true,
 	})
+
+	useEffect(() => {
+		fetch('http://localhost:3000/api/users')
+		.then(response => response.json())
+		.then(data => console.log(data))
+	}, [])
 
 	return (
 		<Box>
