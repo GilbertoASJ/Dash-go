cd666addcd8c9a8d98e7688c4cfd97d22fe3a2f3
GilbertoASJ <alvesgilberto84@gmail.com>
2021-05-31 17:11:40 -0300

    :sparkles: Updating project

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -23,7 +23,20 @@ export default function UserList() {
 		const response = await fetch('http://localhost:3000/api/users')
 		const data = await response.json()
 	
-		return data;
+		const users = data.users.map(user => {
+			return (
+				id: user.id,
+				name: user.name,
+				email: user.email,
+				createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+					day: '2-digit',
+					month: 'long',
+					year: 'numeric'
+				})
+			);
+		});
+
+		return users;
 	});
 
 	const isWideVersion = useBreakpointValue({
@@ -81,29 +94,33 @@ export default function UserList() {
 							</Thead>
 
 							<Tbody>
-								<Tr>
-									<Td px={["4", "4", "6"]}>
-										<Checkbox colorScheme="blue" />
-									</Td>
-									<Td>
-										<Box>
-											<Text fontWeight="bold">Gilberto Alves</Text>
-											<Text fontSize="sm" color="gray.300">alvesgilberto84@gmail.com</Text>
-										</Box>
-									</Td>
-									{ isWideVersion && <Td>05 de Abril, 2021</Td> }
-									<Td>
-										{ isWideVersion && <Button 
-											as="a" 
-											size="sm" 
-											fontSize="sm" 
-											colorScheme="teal" 
-											leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-										>
-												Editar
-										</Button> }
-									</Td>
-								</Tr>
+								{ data.map(user => {
+									return (
+										<Tr key={user.id}>
+											<Td px={["4", "4", "6"]}>
+												<Checkbox colorScheme="blue" />
+											</Td>
+											<Td>
+												<Box>
+													<Text fontWeight="bold">{user.name}</Text>
+													<Text fontSize="sm" color="gray.300">{user.email}</Text>
+												</Box>
+											</Td>
+											{ isWideVersion && <Td>{user.createdAt}</Td> }
+											<Td>
+												{ isWideVersion && <Button 
+													as="a" 
+													size="sm" 
+													fontSize="sm" 
+													colorScheme="teal" 
+													leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+												>
+														Editar
+												</Button> }
+											</Td>
+										</Tr>
+									)
+								}) }
 							</Tbody>
 						</Table>
 
